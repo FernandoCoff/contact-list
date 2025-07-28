5c4198c25eb33335c2e15f21a611de268f7dd64b
FernandoCoff <fernandocoff@outlook.com>
2025-07-28 09:02:17 -0300

    Alert

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import List from './list'
 
 
 const App = ()=>{
+  alert('Para editar, basta ecrever no campo numero, um numero já existente!')
   return(
     <Provider store={store}>
     <Container childrenOne={<Create/>} childrenTwo={<List/>} />
